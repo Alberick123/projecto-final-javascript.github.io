061a86ccde87e4450feeac9addb7e199236b18d2
Alberick123 <95361704+Alberick123@users.noreply.github.com>
2022-06-21 11:13:10 -0300

    Aplicacion de DOM en el html index. Se modifica el texto de noticias con la informacion del Prompt.

---
+++ javascript2.js
@@ -0,0 +1,5 @@
+nombre = prompt("Hola, ingrese su nombre por favor")
+
+let noticiasTexto = document.getElementById("noticiasTexto")
+
+noticiasTexto.innerText = `Hola ${nombre}. En este video resumimos lo que fue otra gran edicion del Buenos Aires Open. Vivimos momentos inolvidables y queremos agradecerte por formar nuevamente parte de ellos. Los esperamos en la edicion del proximo año.`
