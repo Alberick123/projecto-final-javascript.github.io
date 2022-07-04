1659c73c36d01a2086c4a593c385b9436b06f247
Alberick123 <95361704+Alberick123@users.noreply.github.com>
2022-07-04 13:10:21 -0300

    Se agrego un operador ternario en el archivo javascript del html index

--- javascript2.js
+++ javascript2.js
@@ -3,7 +3,12 @@ let nombre = document.getElementById("inputIndex")
 
 let noticiasTexto = document.getElementById("noticiasTexto")
 
+let fecha = new Date()
+
+let fechaHoras = fecha.getHours()
+
 nombre.onchange = () => {
     nombre = document.getElementById("inputIndex").value
-    noticiasTexto.innerText = `Hola ${nombre}. En este video resumimos lo que fue otra gran edicion del Buenos Aires Open. Vivimos momentos inolvidables y queremos agradecerte por formar nuevamente parte de ellos. Los esperamos en la edicion del proximo año.`
+    const condicionHora = (fechaHoras >= 21 || fechaHoras < 6) ? true : false
+    condicionHora ? noticiasTexto.innerText = `Buenas noches ${nombre}. En este video resumimos lo que fue otra gran edicion del Buenos Aires Open. Vivimos momentos inolvidables y queremos agradecerte por formar nuevamente parte de ellos. Los esperamos en la edicion del proximo año.` : noticiasTexto.innerText = `Buenos dias ${nombre}. En este video resumimos lo que fue otra gran edicion del Buenos Aires Open. Vivimos momentos inolvidables y queremos agradecerte por formar nuevamente parte de ellos. Los esperamos en la edicion del proximo año.`
 }
